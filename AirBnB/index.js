c83c4708a8841f3b70f3a47c952db37b220a0d83
JSH <jsh0210hey@gmail.com>
2024-01-22 17:31:16 +0900

    dd

--- AirBnB/index.js
+++ AirBnB/index.js
@@ -1,7 +1,15 @@
-document.querySelectorAll('.category-button').forEach(button => {
-  button.addEventListener("click", event => {
-    document.querySelector('#active-category-button').removeAttribute('id');
-    button.setAttribute('id', 'active-category-button');
-    // console.log("클릭 발생");
+// document.querySelectorAll('.category-button').forEach(button => {
+//   button.addEventListener("click", event => {
+//     document.querySelector('#active-category-button').removeAttribute('id');
+//     button.setAttribute('id', 'active-category-button');
+//     // console.log("클릭 발생");
+//   })
+// })
+$('.category-button').each(function(index, value){
+  value.addEventListener("click", event => {
+    // console.log('test'); // 디버깅
+    // $('#active-category-button').removeAttribute('id');
+    document.querySelector('#active-category-button').removeAttribute
+    value.setAttribute('id', 'active-category-button');
   })
 })
